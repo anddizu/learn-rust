5b11ff2ac9a9736a0fe1b3b0f9fe5c6e17a4c9d2
zhued <zhuendi@codemao.cn>
2019-10-26 13:26:32 +0800

    Rust中的注释

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
+    // Comments
+
+    // this is an example of a line comment
+    // rugular comments which are ignored by the compiler
+
+    // println!("Hello Rust!")
+
+   /* 
+     * This is another type of comment, a block comment. In general,
+     * line comments are the recommended comment style. But
+     * block comments are extremely useful for temporarily disabling
+     * chunks of code. /* Block comments can be /* nested, */ */
+     * so it takes only a few keystrokes to comment out everything
+     * in this main() function. /*/*/* Try it yourself! */*/*/
+     */
+
+    /*
+    Note: The previous column of `*` was entirely for style. There's
+    no actual need for it.
+    */
+
+    /*
+    Note: The previous column of `*` was entirely for style. There's
+    no actual need for it.
+    */
+
+    // You can manipulate expressions more easily with block comments
+    // than with line comments. Try deleting the comment delimiters
+    // to change the result:
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);    // Is `x` 10 or 100? x = 10
+
     println!("Hello, world!");
 }
