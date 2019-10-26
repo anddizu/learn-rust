3ada2a3336b3e2f2a333d78d35281f59b0d10552
zhued <zhuendi@codemao.cn>
2019-10-26 14:24:11 +0800

    格式化打印输出

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
     // println!("Hello Rust!")
 
-   /* 
+    /*
      * This is another type of comment, a block comment. In general,
      * line comments are the recommended comment style. But
      * block comments are extremely useful for temporarily disabling
@@ -29,7 +29,38 @@ fn main() {
     // than with line comments. Try deleting the comment delimiters
     // to change the result:
     let x = 5 + /* 90 + */ 5;
-    println!("Is `x` 10 or 100? x = {}", x);    // Is `x` 10 or 100? x = 10
+    println!("Is `x` 10 or 100? x = {}", x); // Is `x` 10 or 100? x = 10
 
     println!("Hello, world!");
+
+    //  format!: 格式化文本
+    //  print!: 和format类似，但是文字内容会在控制台打印出来
+    //  println!: 和print!类似，但是会从新的一行开始
+    //  eprint!: 和format类似，但是文字从标准错误中打印
+    //  eprintln!: 和eprint!类似，但是会从新的一行开始
+
+    println!("{} days", 31);
+
+    println!("{0}, this is {1}, {1}, this is {0}", "Rust", "lang");
+
+    println!(
+        "{subject}, {verb}, {object}",
+        subject = "the quick brown fox",
+        verb = "jumps over",
+        object = "the lazy dog",
+    );
+    
+    println!("{} of {:b} people know binary, the other half doesn't,", 1, 2);
+
+    println!("{number:>width$}", 
+        number = 1,
+        width = 10,
+    );
+
+    println!("{number:>0width$}",
+        number = 1,
+        width = 10,
+    );
+
+
 }
